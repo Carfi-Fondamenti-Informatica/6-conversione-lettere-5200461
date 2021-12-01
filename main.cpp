176a83081d78038a2c0223ca47bea6b7cf0d4714
5200461 <91668775+5200461@users.noreply.github.com>
2021-12-01 16:41:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char a=a;
+  cin>>a;
+  if(coversione (a)==false){
+    cout<<"errore";
+  }
+  else{
+    cout<<a;
+  }
   return 0;
 }
